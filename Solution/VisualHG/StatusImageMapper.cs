1786143e15b9de42fd368982fd1cd8cd2361de03
Dmitry Popov <lemon@yandex.ru>
2013-11-15 19:02:33 +0400

    Cleaned up ImageMapper

--- Solution/VisualHG/StatusImageMapper.cs
+++ Solution/VisualHG/StatusImageMapper.cs
@@ -1,50 +1,16 @@
 ﻿using System;
-using System.Text;
-using System.Windows.Forms;
-using System.Drawing;
-using System.Collections;
-using System.IO;
 using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Reflection;
+using System.Windows.Forms;
 
 namespace VisualHg
 {
-    /// <summary>
-    /// This enum specified the usage of glyph icons by VisualHg
-    /// </summary>
-    /// <remarks>This enum contains 16 members which should map the VsStateIcon class if possible</remarks>
-    public enum HgGlyph
+    public class ImageMapper
     {
-        /// <summary>
-        /// Path is versioned and modified (STATEICON_CHECKEDOUT /0x2)
-        /// </summary>
-        Modified,
-
-        /// <summary>
-        /// Item is versioned and unmodified (STATEICON_READONLY /0x6)
-        /// </summary>
-        Normal,
-
-        /// <summary>
-        /// File is versioned; but is not available on disk (STATEICON_DISABLED /0x7)
-        /// </summary>
-        FileMissing,
-
-        /// <summary>
-        /// File has been added but was never committed before (Last+1 /0xC)
-        /// </summary>
-        Added,
-
-        /// <summary>
-        /// File is in conflict; must be resolved before continuing (Last+3 /0xE)
-        /// </summary>
-        InConflict,
-    }
-
-    sealed class ImageMapper
-    {
-        ImageList _statusImageList;
-        ImageList _menuImageList;
+        private ImageList _statusImageList;
+        private ImageList _menuImageList;
 
         public ImageList StatusImageList
         {
@@ -53,13 +19,13 @@ namespace VisualHg
 
         public ImageList MenuImageList
         {
-          get { return _menuImageList ?? (_menuImageList = CreateMenuImageList()); }
+            get { return _menuImageList ?? (_menuImageList = CreateMenuImageList()); }
         }
 
         public ImageList CreateStatusImageList()
         {
             return CreateImageList("StatusIcons.bmp", 7);
-         }
+        }
 
 
         public ImageList CreateMenuImageList()
@@ -70,15 +36,17 @@ namespace VisualHg
 
         private static ImageList CreateImageList(string fileName, int imageWidth)
         {
-            using (Stream images = GetImageStream(fileName))
+            using (var imageStream = GetImageStream(fileName))
             {
-                if (images == null)
+                if (imageStream == null)
+                {
                     return null;
+                }
 
-                Image image = Image.FromStream(images, true, true);
-                Bitmap bitmap = (Bitmap)image;
+                var image = Image.FromStream(imageStream, true, true);
+                var bitmap = (Bitmap)image;
 
-                ImageList imageList = new ImageList();
+                var imageList = new ImageList();
 
                 imageList.ImageSize = new Size(imageWidth, bitmap.Height);
                 bitmap.MakeTransparent(bitmap.GetPixel(0, 0));
@@ -98,8 +66,9 @@ namespace VisualHg
 
         private static Stream GetImageStream(string fileName)
         {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            string imagePath = Path.Combine(Path.GetDirectoryName(assembly.Location), fileName);
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyDirectory = Path.GetDirectoryName(assembly.Location);
+            var imagePath = Path.Combine(assemblyDirectory, fileName);
 
             if (File.Exists(imagePath))
             {
