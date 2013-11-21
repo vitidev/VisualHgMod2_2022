0c8f33386434a319993405e97d324a5279a81fb2
Dmitry Popov <lemon@yandex.ru>
2013-11-22 02:30:14 +0400

    Version 2.0.0.1

--- Solution/VisualHG/Properties/AssemblyInfo.cs
+++ Solution/VisualHG/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("8a8076ee-608c-48f1-ad3c-610ceec5585a")]
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
