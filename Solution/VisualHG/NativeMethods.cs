ab8eba1772fb1add8632e0a5a9b373a5287301fe
Dmitry Popov <lemon@yandex.ru>
2013-11-22 02:39:12 +0400

    Cleaned up NativeMethods

--- Solution/VisualHG/NativeMethods.cs
+++ Solution/VisualHG/NativeMethods.cs
@@ -6,28 +6,6 @@ namespace VisualHg
     internal static class NativeMethods
     {
         [DllImport("user32.dll")]
-        internal static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("user32.dll", EntryPoint = "SendMessage")]
-        internal static extern IntPtr SendMessageITEM(IntPtr hWnd, int msg, IntPtr wParam, ref HDITEM lParam);
-        
-        [DllImport("user32.dll")]
         internal static extern bool SetWindowText(IntPtr hWnd, string lpString);
-
-
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct HDITEM
-        {
-            public int mask;
-            public int cxy;
-            [MarshalAs(UnmanagedType.LPTStr)]
-            public string pszText;
-            public IntPtr hbm;
-            public int cchTextMax;
-            public int fmt;
-            public int lParam;
-            public int iImage;
-            public int iOrder;
-        };
     }
 }
